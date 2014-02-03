439e192dfe5893e9bbd3350f920401ea3cdb52c0
Wen Lin <wenching.lin@zementis.com>
2014-02-03 08:41:01 -0800

    data reader

---
+++ easyminer-io/src/main/java/org/easyminer/io/CSVReader.java
@@ -0,0 +1,29 @@
+package org.easyminer.io;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Read the csv file into a BufferReader.
+ * 
+ * @author wenlin
+ */
+public final class CSVReader {
+
+    public static BufferedReader read(String filename) {
+
+        InputStream instream;
+        try {
+            instream = new FileInputStream(filename);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(instream));
+            return reader;
+        } catch (FileNotFoundException e) {
+            System.out.println(filename + " not found");
+        }
+
+        return null;
+    }
+}
